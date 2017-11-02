c7fd75787e7eeae4ed474a5016b23d61df824c3d
PRINCE VERMA <princeverma@PRINCEs-MacBook-Pro.local>
2017-11-02 16:22:25 +0530

    Uploader chunks

---
+++ UploaderChunks/ApiHandler.swift
@@ -0,0 +1,13 @@
+//
+//  ApiHandler.swift
+//  UploaderChunks
+//
+//  Created by PRINCE VERMA on 25/10/17.
+//  Copyright © 2017 PRINCE VERMA. All rights reserved.
+//
+
+import UIKit
+
+class ApiHandler: NSObject {
+
+}
---
+++ UploaderChunks/Video Api.swift
@@ -0,0 +1,13 @@
+//
+//  Video Api.swift
+//  UploaderChunks
+//
+//  Created by PRINCE VERMA on 02/11/17.
+//  Copyright © 2017 PRINCE VERMA. All rights reserved.
+//
+
+import UIKit
+
+class Video_Api: NSObject {
+
+}
